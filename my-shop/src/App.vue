391fd00f0648295ed38b074913c554048920978d
Appostolics <pavlokeeser@gmail.com>
2025-09-04 19:11:18 +0300

    пкепкуппу

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -1,20 +1,17 @@
 <template>
 	<div class="app">
-		<ProductList/>
-		<ReviewsComponent/>
+		<router-view></router-view>
 		<FooterComponent/>
 	</div>
 </template>
 
 <script>
-import ReviewsComponent from './components/ReviewsComponent.vue';
+
 import FooterComponent from './components/FooterComponent.vue';
-import ProductList from './components/ProductList.vue'; 
+
 
 export default {
 	components:{
-		ProductList,
-		ReviewsComponent,
 		FooterComponent
 	}
 };
---
+++ my-shop/src/pages/AdminDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/admin/products">Головна</router-link>
+            <router-link to="/admin/reviews">Відгуки</router-link>
+        </nav>
+        <main>
+            <router-view /> 
+        </main>
+    </div>
+</template>
+<script>
+    export default{
+        name:'AdminDashboard',
+    };
+</script>
